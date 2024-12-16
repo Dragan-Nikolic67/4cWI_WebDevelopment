2bf2b1d2d91c2307eb2961cf60100eca1857e6c6
Dragan-Nikolic67 <nesamskolovao2@gmail.com>
2024-12-16 15:18:47 +0100

    Finished JS 01 variables

---
+++ Javascript/script.js
@@ -0,0 +1,50 @@
+console.log("hallo");
+
+let a= 7;
+let b = 8;
+
+console.log("A hat den Wert " + a);
+
+a = 10;
+
+console.log("A hat den Wert " + a);
+
+let c = a + b;
+
+console.log(c);
+
+a = 7
+b = 8
+c = 10
+
+
+
+// Berechne folgendes und gib das Eregebnis über console.log aus
+// a + b + c
+console.log(a + b + c);
+// (a + b) / c
+console.log((a + b) / c);
+// c -a + b
+console.log(c - a + b);
+// a + b + c * 3
+console.log(a + b + c * 3);
+// (a + b + c)/4
+console.log((a + b + c) / 4);
+
+// Ändere die Variablen auf folgende Werte
+a = 3
+b = a + 3
+c = c -3
+// gib die neuen Werte der Variablen aus
+
+// a + b + c
+console.log(a + b + c);
+// (a + b) / c
+console.log((a + b) / c);
+// c -a + b
+console.log(c - a + b);
+// a + b + c * 3
+console.log(a + b + c * 3);
+// (a + b + c)/4
+console.log((a + b + c) / 4);
+
