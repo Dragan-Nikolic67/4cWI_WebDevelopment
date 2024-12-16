0f713450af908570ec8fb3ed808c1833ba56bb30
Dragan-Nikolic67 <nesamskolovao2@gmail.com>
2024-12-16 15:26:20 +0100

    Finished 02 Strings & Zahlen

---
+++ Javascript/JS_02_Strings_Zahlen/script.js
@@ -0,0 +1,10 @@
+let a = 17.4;
+let b = 3.9;
+let c = a / b;
+
+console.log("The result of " + a + " divided by " + b + " is " + c);
+
+let firstname = "Dragan";
+let lastname = "Nikolic"
+
+console.log("Ich heiße " + firstname + " " + lastname)
