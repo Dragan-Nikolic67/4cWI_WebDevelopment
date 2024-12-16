81dac33521b7d34062005710ddabe025fda117e9
Dragan-Nikolic67 <nesamskolovao2@gmail.com>
2024-12-16 16:36:03 +0100

    Finished 04 for loops

---
+++ Javascript/JS_04_Loops/script.js
@@ -0,0 +1,37 @@
+for (let count = 7; count < 46; count ++){
+    console.log(count)
+    
+}
+
+for (let count = 9; count < 100; count ++){
+    console.log(count)
+    
+}
+
+for (let count = 10; count > -1; count --){
+    console.log(count)
+    
+}
+
+for (let count = 1234; count > 1206; count --){
+    console.log(count)
+    
+}
+
+for (let count = 0; count < 8; count ++){
+    console.log("Dragan")
+    
+}
+
+for (let count = 0; count <= 10; count++) {
+    console.log(count);
+
+}
+
+
+console.log("Dragan");
+
+
+for (let count = 10; count >= 0; count--) {
+    console.log(count);
+}
