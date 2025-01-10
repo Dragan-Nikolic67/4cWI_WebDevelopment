aa89ef1c658606a6a97f6aa1c1c6feeedef3f37c
Dragan-Nikolic67 <nesamskolovao2@gmail.com>
2025-01-10 09:33:16 +0100

    Finished Switch statement

---
+++ Javascript/JS_08_Switch/script.js
@@ -0,0 +1,23 @@
+let randomNumber = Math.random() * 6 + 5;
+let random = Math.floor(randomNumber);
+
+switch (random){
+    case 10:
+        console.log("ten");
+        break;
+    case 9:
+        console.log("nine");
+        break;
+    case 8:
+        console.log("eight");
+        break;
+    case 7:
+        console.log("seven");
+        break;
+    case 6:
+        console.log("six");
+        break;
+    case 5:
+        console.log("five");
+        break;
+}
