2ddb1e7bb807b609703e03463fe57191c8a8522a
Dragan-Nikolic67 <nesamskolovao2@gmail.com>
2025-01-10 09:06:30 +0100

    Finished 05 if

---
+++ Javascript/JS_05_If/script.js
@@ -0,0 +1,17 @@
+let number = 7;
+
+if (number<20){
+    console.log("Mini")
+} else if (number > 20 && number < 50){
+    console.log("Medium")
+} else {
+    console.log("large")
+}
+
+if (number<=7){
+    console.log("Mini")
+} else if (number > 7 && number <= 90){
+    console.log("Medium")
+} else {
+    console.log("large")
+}
